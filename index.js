708db0a2945a65649a88008e5ffea0a6e9b15b9a
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-04-12 16:51:53 +0200

    delete method done

--- index.js
+++ index.js
@@ -1,89 +1,132 @@
-//object express, function require
-const express = require('express');
-//object app, function express
+//GET, POST, PUT and DELETE methods take 2 arguments. First the path/URL.
+//The second is a callbackfunction that will be called when we have an http-request to the URL endpoint.
+//The callback have two arguments, request and response.
+
+
+//Load the express modual. Return function with name express.
+const express = require("express");
+
+//Object app, call the function express.
 const app = express();
 
-//adding middleware, we can write app.use to use the middleware
-app.use(express.json())
+//PORT. assign the port for the app.
+//Use the environment variable PORT, else port 3000.
+//console.log too se which port are in use.
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
 
+//Adding middleware, we can write app.use to use the middleware
+app.use(express.json());
+
+//The resource, a array of objects.
 const dogs = [
- {
-   id: 1,
-   name: "Tuff",
-   born: 2012, 
-   breed: "Dachshund"
- },
+  {
+    id: 1,
+    name: "Tuff",
+    born: 2012,
+    breed: "Dachshund",
+  },
 
- {
+  {
     id: 2,
     name: "Otto",
-    born: 2015, 
-    breed: "Weimaraner"
-    
+    born: 2015,
+    breed: "Weimaraner",
   },
 
   {
     id: 3,
     name: "Uno",
-    born: 2017, 
-    breed: "Italian greyhound"
+    born: 2017,
+    breed: "Italian greyhound",
   },
 
   {
     id: 4,
     name: "Inka",
-    born: 2019, 
-    breed: "Perro sin pelo del perú"
-    
-  }
+    born: 2019,
+    breed: "Perro sin pelo del perú",
+  },
+];
 
-]
-//get method. Impliment endpoints that respond to http get request.
-//this method takes 2 arguments, first the path/URL. 
-//The second is a callbackfunction. 
-//This function will be called when we have an http get-request to the URL endpoint '/'
-//the callback have two arguments, request and response
-//define route '/' and a route-handler(the callback function)
-app.get('/', (req, res) => {
-res.send('Hello world');
+//GET METHOD.
+//Get the list of dog objects and send it to the client.
+app.get("/api/dogs", (req, res) => {
+  res.send(dogs);
 });
 
-//get the list of objects and send it to the client
-app.get('/api/dogs', (req, res) => {
-res.send(dogs);
+//GET METHOD, Route.
+  //find the dog object with the id that matches the id in URL.
+//store the dog object with the matching id and send it to the client.
+app.get("/api/dogs/:id", (req, res) => {
+  const dog = dogs.find((d) => d.id === parseInt(req.params.id));
+  //If there is no id match, return 404 to the client.
+  if (!dog) return res.status(404).send("The dog with the given ID was not found.");
+  res.send(dog);
 });
 
+//POST METHOD.
+//Create a new dog object to our array in server
+app.post("/api/dogs", (req, res) => {
 
-//post method. create a new object to our array in server
-//this method takes 2 arguments, first the path/URL. 
-//The second is a callbackfunction. 
-//This function will be called when we have an http get-request to the URL endpoint '/'
-//the callback have two arguments, request and response
-//define route '/' and a route-handler(the callback function)
-app.post('/api/dogs', (req, res) => {
-    const dog = {
-       id: dogs.array + 1,
-       name: req.body.name,
-       born: req.body.born,
-       breed: req.body.breed
-    };
-    //push the object to the array
-    dogs.push(dog)
-    //send the object to the client
-    res.send(dog)
+/*   //validation, if name, born or breed exist or not
+  if (!req.body.name || !req.body.born || !req.body.breed) 
+    //send this error message to the client
+   return res.status(400).send('Name, year of birth and breed is required.');
+   */
+
+  //define how the object will look like
+  const dog = {
+    id: dogs.length + 1,
+    name: req.body.name,
+    born: req.body.born,
+    breed: req.body.breed,
+  };
+  //push the object to the array
+  dogs.push(dog);
+  //send the object to the client
+  res.send(dog);
+});
+
+//PUT METHOD.
+//For updating a dog object.
+app.put("/api/dogs/:id", (req, res) => {
+  //find the dog object with the id that matches the id in URL.
+  //if the dog object doesnt exist, return 404
+  const dog = dogs.find((d) => d.id === parseInt(req.params.id));
+  if (!dog) return res.status(404).send("The dog with the given ID was not found.");
+
+/*   //validation, if name, born or breed exist or not
+  if (!req.body.name || !req.body.born || !req.body.breed) 
+    //send this error message to the client
+   return res.status(400).send('Name, year of birth and breed is required.');
+   */
+
+  //update the dog object
+  dog.name = req.body.name;
+  dog.born = req.body.born;
+  dog.breed = req.body.breed;
+
+  //return the updated dog object to the client
+  res.send(dog);
+});
+
+//DELETE METHOD. 
+//For deleting a dog object.
+app.delete("/api/dogs/:id", (req, res) => {
+//find the dog object with the id that matches the id in URL.
+  //if the dog object doesnt exist, return 404
+  const dog = dogs.find((d) => d.id === parseInt(req.params.id));
+  if (!dog) return res.status(404).send("The dog with the given ID was not found.");
+
+  //find the index of the dog object we want to delete in our dog array.
+  const index = dogs.indexOf(dog)
+  //delete the object
+  dogs.splice(index, 1)
+
+   //return the updated dog object to the client
+   res.send(dog)
+
 })
 
-//get the objects that matches the id in URL to the value of id for the object. 
-//store id in dog and send it to the client.
-//api/dogs/1 will give you id 1
-//if we dont find a dog with value id that matches
-app.get('/api/dogs/:id', (req, res) => {
-const dog = dogs.find( d => d.id === parseInt(req.params.id))
-if (!dog) res.status(404).send('The dog with the given ID was not found.')
-res.send(dog)
-})
 
-
-// PORT. assign the port, use the env. port, else 3000
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}`));
