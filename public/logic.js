c898b287d03c61edc9eea645bee09d86a796f753
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-04-14 22:11:27 +0200

    print all dogs info on page

---
+++ public/logic.js
@@ -0,0 +1,28 @@
+
+fetch("http://localhost:3000/api/dogs").then((response) => {
+return response.json()
+}).then((dogs) => {
+    console.log(dogs)
+    printAllDogs(dogs)
+})
+
+function printAllDogs(dogs) {
+    let allDogsContainer = document.getElementById("allDogs")
+dogs.forEach(dog => {
+    
+    let dogName = document.createElement("h4")
+    dogName.innerText = dog.name
+    let dogBorn = document.createElement("h4")
+    dogBorn.innerText = dog.born
+    let dogBreed = document.createElement("h4")
+    dogBreed.innerText = dog.breed
+
+    let dogDiv = document.createElement("div")
+    dogDiv.appendChild(dogName)
+    dogDiv.appendChild(dogBorn)
+    dogDiv.appendChild(dogBreed)
+
+    allDogsContainer.appendChild(dogDiv)
+
+});
+}
